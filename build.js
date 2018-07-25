729b0d31cd6d7708c4449f8a313058b9c2229a3d
Radoslaw Weidemann <walecznyopos@gmail.com>
2018-07-25 23:22:27 +0200

    add images

--- build.js
+++ build.js
@@ -123,7 +123,7 @@ var User = function (_React$Component3) {
             return React.createElement(
                 "div",
                 null,
-                React.createElement("img", { src: this.props.avatar_url, style: { maxWidth: '100px' } }),
+                React.createElement("img", { src: this.props.user.avatar_url, style: { maxWidth: '100px' } }),
                 React.createElement(
                     "a",
                     { href: this.props.user.html_url, target: "_blank" },
--- script.js
+++ script.js
@@ -64,7 +64,7 @@ class User extends React.Component{
     render(){
         return(
             <div>
-                <img src={this.props.avatar_url} style={{maxWidth: '100px'}} />
+                <img src={this.props.user.avatar_url} style={{maxWidth: '100px'}} />
                 <a href={this.props.user.html_url} target="_blank"> {this.props.user.login}</a>
             </div>    
         )
